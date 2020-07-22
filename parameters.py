061d9e4625ca722f13f365e6f5672144f17aea30
Rajat Ghosh <rajatghosh@Rajats-MacBook-Air.local>
2020-07-22 12:56:18 -0700

    add figures

---
+++ parameters.py
@@ -0,0 +1,4 @@
+reference_date='2020-03-16' 
+current_date='2020-07-20'
+moveback_date='2019-10-15'
+
---
+++ src/.ipynb_checkpoints/static-checkpoint.py
@@ -0,0 +1,10 @@
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 20,
+        }
+
+filepaths = ['../data/SPLK.csv', '../data/DDOG.csv', '../data/DT.csv', '../data/NEWR.csv']
+APM_Companies = ['Splunk', 'Datadog', 'Dynatrace', 'New Relic']
+colors = ['#E67E22', '#A569BD', '#58D68D', '#D4AC0D']
+refer = dict(zip(filepaths, APM_Companies))
