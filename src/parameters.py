b4d1fb9e55691d5b897f35046ba7d0154383a33f
Rajat Ghosh <rajatghosh@Rajats-MacBook-Air.local>
2020-07-24 14:35:47 -0700

    update parameters static

--- src/parameters.py
+++ src/parameters.py
@@ -1,3 +1,9 @@
+start_date='2019-07-23'
+moveback_date='2019-10-15'
 reference_date='2020-03-16' 
 current_date='2020-07-20'
-moveback_date='2019-10-15'
+
+colors = ['#E67E22', '#A569BD', '#58D68D', '#D4AC0D']
+dy_param_list = ['2020-03-16', '2020-04-16', '2020-05-15', '2020-06-16', '2020-07-16'] 
+
+bayes_intervals = list(zip(dy_param_list[:-1], dy_param_list[1:], colors))
--- src/static.py
+++ src/static.py
@@ -1,10 +1,31 @@
-font = {'family': 'serif',
-        'color':  'darkred',
-        'weight': 'normal',
-        'size': 20,
+font = {
+        'family':'serif',
+        'color': 'black',
+        'weight':'normal',
+        'size':24,
         }
+
+vline_prop = {
+        'color':'#C0392B',
+        'linestyle':'dashed',
+        'linewidth':4,
+}
+
+test_data_prop = {
+        'plot_title': 'Daily Closing Stock Price ($) for Monitoring Companies',
+}
+
+two_sided_confidence_level = {
+        '99%': 2.576,
+        '98%': 2.326,
+        '95%': 1.96,
+        '90%': 1.645,
+}
 
 filepaths = ['../data/SPLK.csv', '../data/DDOG.csv', '../data/DT.csv', '../data/NEWR.csv']
 APM_Companies = ['Splunk', 'Datadog', 'Dynatrace', 'New Relic']
 colors = ['#E67E22', '#A569BD', '#58D68D', '#D4AC0D']
 refer = dict(zip(filepaths, APM_Companies))
+
+before_prop = {'color': 'b', 'label': 'before/on March 16, 2020'}
+after_prop = {'color': 'r', 'label': 'after March 16, 2020'}
